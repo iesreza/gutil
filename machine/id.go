2d9bc54b10c1ba419025e255ab8ce71176fef5d6
Reza <reza@ies-italia.it>
2019-06-21 14:52:59 +0200

    Update

--- machine/id.go
+++ machine/id.go
@@ -3,14 +3,12 @@ package machine
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"github.com/denisbrodbeck/machineid"
 	"github.com/iesreza/netconfig"
 )
 
 func UniqueID() string {
-	id, _ := machineid.ID()
 	cfg := netconfig.GetNetworkConfig()
-	id += cfg.HardwareAddress.String()
+	id := cfg.HardwareAddress.String()
 	hasher := md5.New()
 	hasher.Write([]byte(id))
 	return hex.EncodeToString(hasher.Sum(nil))
